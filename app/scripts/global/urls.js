da1cdf36007b42621e5d7c9b43aa1ffb124c41e4
suryanarayanswami <suryanarayan@beehyv.com>
2021-01-04 19:25:40 +0530

    changin url for usermanual

--- app/scripts/global/urls.js
+++ app/scripts/global/urls.js
@@ -1,4 +1,4 @@
 // backend_root = 'http://172.17.80.12:9090/WAReportingSuite/'
-backend_root = 'http://localhost:9090/WAReportingSuite/'
-// backend_root = 'https://sbmacademy.co.in/WAReportingSuite/'
+// backend_root = 'http://localhost:9090/WAReportingSuite/'
+backend_root = 'https://sbmacademy.co.in/WAReportingSuite/'
 
